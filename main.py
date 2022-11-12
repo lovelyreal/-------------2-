631e722f28f0776724d7055a582525665b43535f
lovelyreal <lovelyreal@mail.ru>
2022-11-12 20:26:51 +0300

    Задание 4

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-#Задание 3
-a ="А роза упала на лапу Азора"
-c = ""
-b = a.replace(" ","")
-g = b.lower()
-for i in range(len(a)-1, -1, -1):
-    c += a[i]
-t = c.replace(" ","")
-f = t.lower()
-if g == f:
-    print("Да")
-else:
-    print("Нет")
+#Задание 4
+a ="pasdkpaskdaskd;ad;kas;k.asoidaosdhosahdaosjhdaso.aspdapsidapisdj."
+print(a.count("."))
