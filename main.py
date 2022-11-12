f52ed464026a3446baed16ad26901ada8bbd5eaa
lovelyreal <lovelyreal@mail.ru>
2022-11-12 20:23:03 +0300

    Задание 3

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-#Задание 2
-a = input("Введите строку: ")
-b = input("Введите символ: ")
-
-
-print(a.count(b))
-
+#Задание 3
+a ="А роза упала на лапу Азора"
+c = ""
+b = a.replace(" ","")
+g = b.lower()
+for i in range(len(a)-1, -1, -1):
+    c += a[i]
+t = c.replace(" ","")
+f = t.lower()
+if g == f:
+    print("Да")
+else:
+    print("Нет")
