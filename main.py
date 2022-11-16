3e065b639343006ae0646408fc20daa16a3215d0
lovelyreal <lovelyreal@mail.ru>
2022-11-16 21:27:45 +0300

    Homework 1

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-#Задание 4
-a ="pasdkpaskdaskd;ad;kas;k.asoidaosdhosahdaosjhdaso.aspdapsidapisdj."
-print(a.count("."))
+a = input("")
+b = ""
+h = 0
+g = ""
+for i in range(len(a)):
+    if a[i] != "+":
+        b+= a[i]
+
+    elif a[i] == "+":
+        break
+    h += 1
+for i in range(h-len(a),-1,-1):
+    print(a[i])
+print(b)
