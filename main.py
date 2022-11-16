1c3e0d81b7d293a8c651f784865b1c583d1c69ae
lovelyreal <lovelyreal@mail.ru>
2022-11-16 21:37:54 +0300

    Homework 1

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 a = input("")
-b = ""
+b = "" #Первое число
 h = 0
-g = ""
+g = "" #Второе число
 for i in range(len(a)):
     if a[i] != "+":
         b+= a[i]
-
-    elif a[i] == "+":
+    elif a[i] == "+" or "*" or "/" or "-":
+        c = a[i]
         break
     h += 1
-for i in range(h-len(a),-1,-1):
-    print(a[i])
-print(b)
+for i in range(h-len(a)+1,0):
+    g += a[i]
+b = int(b)
+g = int(g)
+if c == "-":
+    print(b-g)
+if c == "*":
+    print(b*g) 
+if c == "/":
+    print(b/g)
+if c == "+":
+    print(b+g)
